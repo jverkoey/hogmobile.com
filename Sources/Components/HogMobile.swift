48cf7b049adc417e7e44847d0f1f83b7b6e4b2c4
Jeff Verkoeyen <jverkoey@gmail.com>
2024-08-13 23:13:26 -0400

    Handle reduce motion.

--- Sources/Components/HogMobile.swift
+++ Sources/Components/HogMobile.swift
@@ -7,12 +7,12 @@ struct HogMobile: View {
     Div {
       Div {
         HogImage(URL(string: "/gfx/hogmobile-light-shadow.png"))
-        HogImage(URL(string: "/gfx/hogmobile-light-hog.png")).className("animate-bounce-hog")
-        HogImage(URL(string: "/gfx/hogmobile-light-rear-wheels.png")).className("animate-bounce-wheels")
-        HogImage(URL(string: "/gfx/hogmobile-light-car.png")).className("animate-bounce-car")
-        HogImage(URL(string: "/gfx/hogmobile-light-front-wheels.png")).className("animate-bounce-wheels")
-        HogImage(URL(string: "/gfx/hogmobile-light-paw.png")).className("animate-bounce-paw")
-        HogImage(URL(string: "/gfx/hogmobile-light-headlights.png")).className("animate-bounce-car")
+        HogImage(URL(string: "/gfx/hogmobile-light-hog.png"))         .className("motion-safe:animate-bounce-hog")
+        HogImage(URL(string: "/gfx/hogmobile-light-rear-wheels.png")) .className("motion-safe:animate-bounce-wheels")
+        HogImage(URL(string: "/gfx/hogmobile-light-car.png"))         .className("motion-safe:animate-bounce-car")
+        HogImage(URL(string: "/gfx/hogmobile-light-front-wheels.png")).className("motion-safe:animate-bounce-wheels")
+        HogImage(URL(string: "/gfx/hogmobile-light-paw.png"))         .className("motion-safe:animate-bounce-paw")
+        HogImage(URL(string: "/gfx/hogmobile-light-headlights.png"))  .className("motion-safe:animate-bounce-car")
           .className("active:opacity-0")
           .animation(.easeInOut)
       }
@@ -20,12 +20,12 @@ struct HogMobile: View {
 
       Div {
         HogImage(URL(string: "/gfx/hogmobile-dark-shadow.png"))
-        HogImage(URL(string: "/gfx/hogmobile-dark-hog.png")).className("animate-bounce-hog")
-        HogImage(URL(string: "/gfx/hogmobile-dark-rear-wheels.png")).className("animate-bounce-wheels")
-        HogImage(URL(string: "/gfx/hogmobile-dark-car.png")).className("animate-bounce-car")
-        HogImage(URL(string: "/gfx/hogmobile-dark-front-wheels.png")).className("animate-bounce-wheels")
-        HogImage(URL(string: "/gfx/hogmobile-dark-paw.png")).className("animate-bounce-paw")
-        HogImage(URL(string: "/gfx/hogmobile-dark-headlights.png")).className("animate-bounce-car")
+        HogImage(URL(string: "/gfx/hogmobile-dark-hog.png"))          .className("motion-safe:animate-bounce-hog")
+        HogImage(URL(string: "/gfx/hogmobile-dark-rear-wheels.png"))  .className("motion-safe:animate-bounce-wheels")
+        HogImage(URL(string: "/gfx/hogmobile-dark-car.png"))          .className("motion-safe:animate-bounce-car")
+        HogImage(URL(string: "/gfx/hogmobile-dark-front-wheels.png")) .className("motion-safe:animate-bounce-wheels")
+        HogImage(URL(string: "/gfx/hogmobile-dark-paw.png"))          .className("motion-safe:animate-bounce-paw")
+        HogImage(URL(string: "/gfx/hogmobile-dark-headlights.png"))   .className("motion-safe:animate-bounce-car")
           .className("active:opacity-0")
           .animation(.easeInOut)
       }
@@ -35,6 +35,6 @@ struct HogMobile: View {
     .position(.relative)
     .frame(width: 200, height: 150)
     .frame(width: 400, height: 300, condition: .startingAt(.medium))
-    .className("animate-drive-car")
+    .className("motion-safe:animate-drive-car")
   }
 }
